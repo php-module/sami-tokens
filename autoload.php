d55dbbc833917e2fbe9fe81cc34138e389c292b9
agostinhosamil <agostinhosaml832@gmail.com>
2021-07-29 09:23:48 +0100

    root: init

---
+++ autoload.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * @version 2.0
+ * @author Sammy
+ *
+ * @keywords Samils, ils, php framework
+ * -----------------
+ * @namespace Sammy\Packs\SamiTokens
+ * - Autoload, application dependencies
+ */
+namespace Sammy\Packs\SamiTokens {
+  $autoloadFile = __DIR__ . '/vendor/autoload.php';
+
+  if (is_file ($autoloadFile)) {
+    include_once $autoloadFile;
+  }
+}
